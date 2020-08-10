401a5b4ce209f11754b5996abc6672ee9577ef42
Leslie G <lesliegonzalez@me.com>
2020-08-10 01:34:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,14 @@
 class Anagram
   
-attr_accessor = :word
+attr_accessor :word
 
 def initialize(word)
-  
+  @word = word
+end
+
+def anagram_detector(array)
+  words.match.do |w|
+    if w.match
 
 
 
